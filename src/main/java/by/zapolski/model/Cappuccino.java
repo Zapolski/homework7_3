def681d186287de6271b019ca9ff65fde3cf0c51
Zapolsky <nestor.s@mail.ru>
2019-01-16 13:49:06 +0300

    develop

---
+++ src/main/java/by/zapolski/model/Cappuccino.java
@@ -0,0 +1,4 @@
+package by.zapolski.model;
+
+public class Cappuccino {
+}
---
+++ src/main/java/by/zapolski/model/CoffeeMachineInterface.java
@@ -0,0 +1,7 @@
+public interface CoffeeMachineInterface {
+    void enable();
+    void disable();
+    void makeCappuccino();
+    void makeEspresso();
+    void tankC
+}
